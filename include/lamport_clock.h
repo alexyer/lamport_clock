f0bd45ea925185bbbe42076a48be3e069e1404a0
Olexander Yermakov <mannavard1611@gmail.com>
2017-01-28 14:50:34 +0200

    Implement Lamport Clock

--- include/lamport_clock.h
+++ include/lamport_clock.h
@@ -1,4 +1,71 @@
 #ifndef LAMPORT_CLOCK_LAMPORT_CLOCK_H
 #define LAMPORT_CLOCK_LAMPORT_CLOCK_H
 
+#include <atomic>
+
+class LamportClock {
+public:
+    /// Lamport timestamp
+    typedef unsigned int LamportTime;
+
+    LamportClock() : time_(0) {}
+
+    /**
+     * Get current Lamport timestamp.
+     *
+     * @return LamportTime value.
+     */
+    LamportTime get_time() const {
+        return time_.load();
+    }
+
+    /**
+     * Handle local event.
+     * Increment timer and return the new value.
+     *
+     * @return LamportTime value;
+     */
+    LamportTime local_event() {
+        return time_.fetch_add(1);
+    }
+
+    /**
+     * Handle send event.
+     * Increment local time and return the new value.
+     *
+     * @return LamportTime value;
+     */
+    LamportTime send_event() {
+        return time_.fetch_add(1);
+    }
+
+    /**
+     * Handle receive event.
+     * Receive sender's time and set the local time to the maximum between received and local time plus 1.
+     *
+     * @param received_time Sender's time.
+     * @return LamportTime value;
+     */
+    LamportTime receive_event(LamportTime received_time) {
+        RECEIVE_EVENT:
+
+        auto cur = get_time();
+
+        // If received time is old, do nothing.
+        if (received_time < cur) {
+            return cur;
+        }
+
+        // Ensure that local timer is at least one ahead.
+        if (!time_.compare_exchange_strong(cur, received_time + 1)) {
+            goto RECEIVE_EVENT;
+        }
+
+        return received_time + 1;
+    }
+
+private:
+    std::atomic<LamportTime> time_;
+};
+
 #endif //LAMPORT_CLOCK_LAMPORT_CLOCK_H
--- tests/lamport_clock_tests/lamport_clock_tests.cpp
+++ tests/lamport_clock_tests/lamport_clock_tests.cpp
@@ -1,1 +1,29 @@
 #include <gtest/gtest.h>
+#include "../../include/lamport_clock.h"
+
+TEST(LamportClock_Test, InitWithZero) {
+    LamportClock clock;
+    ASSERT_EQ(clock.get_time(), 0);
+}
+
+TEST(LamportClock_Test, LocalEvent) {
+    LamportClock clock;
+    clock.local_event();
+    ASSERT_EQ(clock.get_time(), 1);
+}
+
+TEST(LamportClock_Test, SendEvent) {
+    LamportClock clock;
+    clock.send_event();
+    ASSERT_EQ(clock.get_time(), 1);
+}
+
+TEST(LamportClock_Test, ReceiveEvent) {
+    LamportClock clock;
+
+    clock.receive_event(3);
+    ASSERT_EQ(clock.get_time(), 4);
+
+    clock.receive_event(2);
+    ASSERT_EQ(clock.get_time(), 4);
+}
